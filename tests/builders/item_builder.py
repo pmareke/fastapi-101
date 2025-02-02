07e5c706dc8af296179ad2703dbfddd17ec7211f
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-02-02 10:09:46 +0100

    feat: refactor code

--- tests/builders/item_builder.py
+++ tests/builders/item_builder.py
@@ -1,15 +1,10 @@
-from src.domain.item import Item, ItemID
+from src.domain.item import Item
 
 
 class ItemBuilder:
     def __init__(self):
-        self.item_id = ItemID()
         self.name = "Item Name"
         self.value = 100
-
-    def with_item_id(self, item_id: ItemID) -> "ItemBuilder":
-        self.item_id = item_id
-        return self
 
     def with_name(self, name: str) -> "ItemBuilder":
         self.name = name
@@ -20,4 +15,4 @@ class ItemBuilder:
         return self
 
     def build(self) -> Item:
-        return Item(self.item_id, self.name, self.value)
+        return Item(self.name, self.value)
